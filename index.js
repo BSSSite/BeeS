f430bcb1788fb43728b6e26fd69394768cc66cc4
BSSSite <157333377+BSSSite@users.noreply.github.com>
2024-01-28 19:40:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const Port = 3000;
 
 
 App.post("/api/v1/upload/decal", async (req, res) => {
+    const Cookie = req.body.cookie
+    const currentUser = await noblox.setCookie(Cookie) 
+    res.send(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
 
-  await noblox.uploadItem("A cool decal.", 13, "https://cdn.vectorstock.com/i/preview-1x/99/27/pointer-finger-click-icon-vector-24389927.jpg")
+
+  // await noblox.uploadItem("A cool decal.", 13, "https://cdn.vectorstock.com/i/preview-1x/99/27/pointer-finger-click-icon-vector-24389927.jpg")
 
   
-  res.send(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
 
 
   
